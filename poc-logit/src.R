c62ab88b47de846716b98c6df75e60b85b0730a8
jaehyeon-kim <dottami@gmail.com>
2017-04-07 06:22:26 +1000

    model saved

---
+++ poc-logit/src.R
@@ -0,0 +1,12 @@
+# http://stats.idre.ucla.edu/r/dae/logit-regression/
+data <- read.csv("http://www.ats.ucla.edu/stat/data/binary.csv")
+data$rank <- as.factor(data$rank)
+
+fit <- glm(admit ~ ., data = data, family = "binomial")
+
+#newdata <- data.frame(gre = max(data$gre), gpa = max(data$gpa), rank = c("1", "2"))
+#predict(fit, newdata = newdata, type = "response")
+
+saveRDS(fit, "admission.rds")
+
+
